13eef47c6b5750b8dd3d7c3ba9670d675fab85fb
Vittorio Polto <vittorio.polto@axant.it>
2018-12-05 15:53:58 +0100

    More coverage test, now 80%

--- resetpassword/helpers.py
+++ resetpassword/helpers.py
@@ -2,9 +2,3 @@
 
 """WebHelpers used in tgapp-resetpassword."""
 
-#from webhelpers import date, feedgenerator, html, number, misc, text
-from markupsafe import Markup
-
-
-def bold(text):
-    return Markup('<strong>%s</strong>' % text)
